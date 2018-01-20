f3a1b53c3efa085c5f44eeaa3c37e17faf5d228e
kevin115533 <kevin115533@yahoo.com>
2018-01-20 13:06:06 -0600

    Initial commit

---
+++ business_card.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char something;
+	cout << "========================================\n";
+	cout << "           _    _  _   _   ______      \n";
+	cout << "          | | / / | | / / |__  __|     \n";
+	cout << "          | |/ /  | |/ /    | |        \n";
+	cout << "          | |\\ \\  | |\\ \\    | |        \n";
+	cout << "          |_| \\_\\ |_| \\_\\   |_|        \n";
+	cout << "               Kevin K Tran           \n";
+	cout << "               405.926.0131 \n";
+	cout << "           kevin115533@yahoo.com \n";
+	cout << "========================================\n";
+	return 0;
+}
