4d5df66bd676c5446bd83aa4a4f9dd676954c39e
kevin115533 <kevin115533@yahoo.com>
2018-01-22 14:46:59 -0600

    business_card.cpp

--- business_card.cpp
+++ business_card.cpp
@@ -3,15 +3,18 @@ using namespace std;
 
 int main(){
 	char something;
-	cout << "========================================\n";
-	cout << "           _    _  _   _   ______      \n";
-	cout << "          | | / / | | / / |__  __|     \n";
-	cout << "          | |/ /  | |/ /    | |        \n";
-	cout << "          | |\\ \\  | |\\ \\    | |        \n";
-	cout << "          |_| \\_\\ |_| \\_\\   |_|        \n";
-	cout << "               Kevin K Tran           \n";
-	cout << "               405.926.0131 \n";
-	cout << "           kevin115533@yahoo.com \n";
-	cout << "========================================\n";
+	cout << "===========================================\n";
+	cout << "             _    _  _   _   ______      \n";
+	cout << "            | | / / | | / / |__  __|     \n";
+	cout << "            | |/ /  | |/ /    | |        \n";
+	cout << "            | |\\ \\  | |\\ \\    | |        \n";
+	cout << "            |_| \\_\\ |_| \\_\\   |_|        \n";
+	cout << "                 Kevin K Tran           \n";
+	cout << "                Student,OSU-OKC  \n";
+	cout << "           Bartender,Ocean 81 Sushi \n";  
+	cout << "                 405.926.0131 \n";
+	cout << "             kevin115533@yahoo.com \n";
+	cout << "            github.com/kevin115533 \n";
+	cout << "============================================\n";
 	return 0;
 }
